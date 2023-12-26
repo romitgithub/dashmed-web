04d3a5e80f4ab1d022da7ad548d04db0fd6307f2
Lokesh Patidar <bansiyalokesh29988@gmail.com>
2023-12-26 10:23:48 +0530

    optimize formcontrol

--- src/views/login/formControl.tsx
+++ src/views/login/formControl.tsx
@@ -1,4 +1,4 @@
-import { useState, FormEvent } from "react";
+import { useState } from "react";
 import { FormControlChecks, LOGIN_TYPES } from ".";
 import { ViewPasswordIcon } from "@/atoms/showPasswordIcon";
 import { HidePasswordIcon } from "@/atoms/hidePasswordIcon";
@@ -34,119 +34,128 @@ export const FormControlSection: React.FC<FormControlSectionProps> = ({ handleSu
           }
      };
 
-     return (
 
-          <form onSubmit={handleSubmitFunc} className='pt-5 pb5'>
-
-               {
-                    loginType === LOGIN_TYPES.MOBILE &&
-                    <>
-                         <label>Enter mobile number</label>
-                         <div className="flex items-center space-x-2">
-                              <input
-                                   type="tel"
-                                   className="p-2 rounded border border-gray-300 flex-1 w-full sm:w-auto md:w-1/2 lg:w-2/3 xl:w-1/2"
-                                   placeholder="Enter mobile number"
-                                   value={formControl?.value}
-                                   name='value'
-                                   onChange={handleInputChange}
-                              />
-                         </div>
-                    </>
-               }
-               {
-                    loginType === LOGIN_TYPES.ABHA_NO &&
-                    <>
-                         <label className="flex w-full">Enter your ABHA Number</label>
-                         <div className="flex w-full items-center space-x-2">
-                              <input
-                                   type="tel"
-                                   className="p-2 rounded border border-gray-300 flex-1 w-full sm:w-auto md:w-1/2 lg:w-2/3 xl:w-1/2"
-                                   placeholder="Enter ABHA Number"
-                                   value={formControl?.value}
-                                   name='value'
-                                   onChange={handleInputChange}
-                                   maxLength={14}
-                                   minLength={14}
-                              />
-                         </div>
-                         <label className="w-full mt-5 flex justify-end text-teal-500 ">Forget ABHA Number?</label>
-                    </>
-               }
-               {
-                    loginType === LOGIN_TYPES.EMAIL &&
-                    <>
-                         <label>Enter your email address</label>
-                         <div className="flex items-center space-x-2">
-                              <input
-                                   type="email"
-                                   className="p-2 rounded border border-gray-300 flex-1 w-full sm:w-auto md:w-1/2 lg:w-2/3 xl:w-1/2"
-                                   placeholder="Example@gmail.com"
-                                   value={formControl?.value}
-                                   name='value'
-                                   onChange={handleInputChange}
-                              />
-                         </div>
-                    </>
-               }
-               {
-                    loginType === LOGIN_TYPES.ABHA_ADD &&
-                    <>
-                         <label>Enter your ABHA address</label>
-                         <div className="rounded border border-gray-300 flex mt-2 items-center space-x-2 mb-5">
-                              <input
-                                   type="email"
-                                   className="p-2 flex-1 w-full sm:w-auto md:w-1/2 lg:w-2/3 xl:w-1/2"
-                                   placeholder="Example@abdm"
-                                   value={formControl?.value}
-                                   name='value'
-                                   onChange={handleInputChange}
-                              />
-                         </div>
-                         <label className="mt-5">Enter your password</label>
-                         <div className="rounded border border-gray-300 flex items-center space-x-2 mt-2 mb-5">
-                              <input
-                                   type={showPassword ? "text" : "password"}
-                                   className="p-2   flex-1 w-full sm:w-auto md:w-1/2 lg:w-2/3 xl:w-1/2"
-                                   placeholder="Enter ABHA password"
-                                   value={formControl?.abhaPassword}
-                                   name='abhaPassword'
-                                   onChange={handleInputChange}
-                                   maxLength={14}
-                                   minLength={14}
-                              />
-                              <span onClick={() => setShowPassword((prev) => !prev)} className="pr-4">
-                                   {!showPassword ? (<ViewPasswordIcon />) : (<HidePasswordIcon />)}
-                              </span>
-                         </div>
-                         <div className="flex w-full justify-between">
-                              <label className="flex items-center space-x-2 text-teal-500">
-                                   <Checkbox label={"Remember me"} id={""} />
-                              </label>
-                              <label className="text-teal-500 text-xs sm:text-sm lg:text-md">Forget ABHA Number?</label>
-                         </div>
-                         <span className="flex justify-center align-middle p-2">Or</span>
-                         <div>
-                              <label className="font-semibold">Validate Using</label>
-                              <div className="flex flex-col">
-                                   <label className="mt-3">
-                                        <input
-                                             type="radio"
-                                             value="Email OTP"
-                                        />
-                                        <span className="pl-3">Email OTP</span>
-                                   </label>
-                                   <label className="mt-3">
-                                        <input
-                                             type="radio"
-                                             value="Mobile OTP"
-                                        />
-                                        <span className="pl-2"> Mobile OTP</span>
-                                   </label>
+     const renderLoginSection = () => {
+          switch (loginType) {
+               case LOGIN_TYPES.MOBILE:
+                    return (
+                         <>
+                              <label>Enter mobile number</label>
+                              <div className="flex items-center space-x-2">
+                                   <input
+                                        type="tel"
+                                        className="p-2 rounded border border-gray-300 flex-1 w-full sm:w-auto md:w-1/2 lg:w-2/3 xl:w-1/2"
+                                        placeholder="Enter mobile number"
+                                        value={formControl?.value}
+                                        name='value'
+                                        onChange={handleInputChange}
+                                   />
                               </div>
-                         </div>
-                    </>
-               }
+                         </>
+                    );
+               case LOGIN_TYPES.ABHA_NO:
+                    return (
+                         <>
+                              <label className="flex w-full">Enter your ABHA Number</label>
+                              <div className="flex w-full items-center space-x-2">
+                                   <input
+                                        type="tel"
+                                        className="p-2 rounded border border-gray-300 flex-1 w-full sm:w-auto md:w-1/2 lg:w-2/3 xl:w-1/2"
+                                        placeholder="Enter ABHA Number"
+                                        value={formControl?.value}
+                                        name='value'
+                                        onChange={handleInputChange}
+                                        maxLength={14}
+                                        minLength={14}
+                                   />
+                              </div>
+                              <label className="w-full mt-5 flex justify-end text-teal-500 ">Forget ABHA Number?</label>
+                         </>
+                    );
+               case LOGIN_TYPES.EMAIL:
+                    return (
+                         <>
+                              <label>Enter your email address</label>
+                              <div className="flex items-center space-x-2">
+                                   <input
+                                        type="email"
+                                        className="p-2 rounded border border-gray-300 flex-1 w-full sm:w-auto md:w-1/2 lg:w-2/3 xl:w-1/2"
+                                        placeholder="Example@gmail.com"
+                                        value={formControl?.value}
+                                        name='value'
+                                        onChange={handleInputChange}
+                                   />
+                              </div>
+                         </>
+                    );
+               case LOGIN_TYPES.ABHA_ADD:
+                    return (
+                         <>
+                              <label>Enter your ABHA address</label>
+                              <div className="rounded border border-gray-300 flex mt-2 items-center space-x-2 mb-5">
+                                   <input
+                                        type="email"
+                                        className="p-2 flex-1 w-full sm:w-auto md:w-1/2 lg:w-2/3 xl:w-1/2"
+                                        placeholder="Example@abdm"
+                                        value={formControl?.value}
+                                        name='value'
+                                        onChange={handleInputChange}
+                                   />
+                              </div>
+                              <label className="mt-5">Enter your password</label>
+                              <div className="rounded border border-gray-300 flex items-center space-x-2 mt-2 mb-5">
+                                   <input
+                                        type={showPassword ? "text" : "password"}
+                                        className="p-2   flex-1 w-full sm:w-auto md:w-1/2 lg:w-2/3 xl:w-1/2"
+                                        placeholder="Enter ABHA password"
+                                        value={formControl?.abhaPassword}
+                                        name='abhaPassword'
+                                        onChange={handleInputChange}
+                                        maxLength={14}
+                                        minLength={14}
+                                   />
+                                   <span onClick={() => setShowPassword((prev) => !prev)} className="pr-4">
+                                        {!showPassword ? (<ViewPasswordIcon />) : (<HidePasswordIcon />)}
+                                   </span>
+                              </div>
+                              <div className="flex w-full justify-between">
+                                   <label className="flex items-center space-x-2 text-teal-500">
+                                        <Checkbox label={"Remember me"} id={""} />
+                                   </label>
+                                   <label className="text-teal-500 text-xs sm:text-sm lg:text-md">Forget ABHA Number?</label>
+                              </div>
+                              <span className="flex justify-center align-middle p-2">Or</span>
+                              <div>
+                                   <label className="font-semibold">Validate Using</label>
+                                   <div className="flex flex-col">
+                                        <label className="mt-3">
+                                             <input
+                                                  type="radio"
+                                                  value="Email OTP"
+                                             />
+                                             <span className="pl-3">Email OTP</span>
+                                        </label>
+                                        <label className="mt-3">
+                                             <input
+                                                  type="radio"
+                                                  value="Mobile OTP"
+                                             />
+                                             <span className="pl-2"> Mobile OTP</span>
+                                        </label>
+                                   </div>
+                              </div>
+                         </>
+                    );
+               default:
+                    return <></>;
+          }
+     };
+
+
+     return (
+          <form onSubmit={handleSubmitFunc} className="pt-5 pb5">
+               {renderLoginSection()}
+               {/* Submit button */}
                <div className='mt-5 mb-5'>
                     <button
                          type="submit"
--- src/views/login/header.tsx
+++ src/views/login/header.tsx
@@ -1,47 +1,55 @@
-import { LeftArrowIcon } from "@/atoms/leftIArrowIcon";
 import { LOGIN_STATES, LOGIN_TYPES } from ".";
+import { LeftArrowIcon } from "@/atoms/leftIArrowIcon";
+
 
 interface HeaderSectionProps {
      loginType: string;
      loginState: string;
-     setLoginType: React.Dispatch<React.SetStateAction<string>>; // Define the type for setLoginType
+     setLoginType: React.Dispatch<React.SetStateAction<string>>;
      setLoginState: React.Dispatch<React.SetStateAction<string>>;
      resetFormControl: () => void;
 };
 
 
-export const HeaderSection: React.FC<HeaderSectionProps> = ({ loginType, loginState, setLoginType, setLoginState, resetFormControl }) => {
+// Constants for login type text
+const loginTypeTextMap: Record<string, string> = {
+     [LOGIN_TYPES?.MOBILE]: "Mobile Number",
+     [LOGIN_TYPES?.ABHA_ADD]: "ABHA address",
+     [LOGIN_TYPES?.ABHA_NO]: "ABHA Number",
+     [LOGIN_TYPES?.EMAIL]: "Email Id",
+};
 
-     const handleBackFromSelectAddressPage = () => {
-          console.log({ 'back': LOGIN_TYPES.MOBILE });
+
+export const HeaderSection: React.FC<HeaderSectionProps> = ({
+     loginType,
+     loginState,
+     setLoginType,
+     setLoginState,
+     resetFormControl,
+}) => {
+     console.log({ LOGIN_TYPES });
+
+     const handleBackButtonClick = () => {
           if (loginState === LOGIN_STATES.ADDRESS_VIEW) {
                setLoginState(LOGIN_STATES.OTP_VIEW);
-          }
-          if (loginState === LOGIN_STATES.OTP_VIEW) {
+          } else if (loginState === LOGIN_STATES.OTP_VIEW) {
                setLoginState(LOGIN_STATES.DEFAULT_VIEW);
-          }
-          if (loginState === LOGIN_STATES.DEFAULT_VIEW) {
+          } else if (loginState === LOGIN_STATES.DEFAULT_VIEW) {
                setLoginType(LOGIN_TYPES.MOBILE);
                resetFormControl();
           }
      };
 
+     const renderLoginType = loginTypeTextMap[loginType] || "Unknown";
+
      return (
-          <>
-               <div className="flex flex-row items-center justify-center relative p-2 w-full">
-                    {
-                         <span className="text-red-500 flex absolute cursor-pointer top-2 left-2 mt-0 ml-0 p-1" onClick={handleBackFromSelectAddressPage}>
-                              <LeftArrowIcon />
-                         </span>
-                    }
-                    <span className="ml-0 font-md sm:text-lg md:text-xl lg:text-2xl xl:text-3xl mb-4">
-                         {`Login with ${(loginType === LOGIN_TYPES.MOBILE && "Mobile Number") ||
-                              (loginType === LOGIN_TYPES.ABHA_ADD && "ABHA address") ||
-                              (loginType === LOGIN_TYPES.ABHA_NO && "ABHA Number") ||
-                              (loginType === LOGIN_TYPES.EMAIL && "Email Id")
-                              }`}
-                    </span>
-               </div>
-          </>
+          <div className="flex flex-row items-center justify-center relative p-2 w-full">
+               <span className="flex absolute cursor-pointer top-2 left-2 mt-0 ml-0 p-1" onClick={handleBackButtonClick}>
+                    <LeftArrowIcon />
+               </span>
+               <span className="ml-0 font-md sm:text-lg md:text-xl lg:text-2xl xl:text-3xl mb-4">
+                    {`Login with ${renderLoginType}`}
+               </span>
+          </div>
      );
 };
