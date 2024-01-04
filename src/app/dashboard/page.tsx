7f8d50dcba86ee6bfb1fefb2efce7c698dc718dd
Lokesh Patidar <bansiyalokesh29988@gmail.com>
2024-01-04 14:10:38 +0530

    add dashboard page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,4 @@
+"use client";
+import withUser from "@/hoc/withUser";
+import DashboardView from "@/views/dashboard";
+export default withUser(DashboardView);
--- src/views/dashboard/index.tsx
+++ src/views/dashboard/index.tsx
@@ -1,4 +1,4 @@
-const ScanView = () => {
+const DashboardView = () => {
      return (
           <div className="flex min-h-screen flex-col items-center w-full small:w-4/5 sm:w-3/5 md:w-2/4 lg:w-2/5 xl:w-2/5 m-auto p-1 mt-10">
                <span className="w-full flex justify-center align-middle font-bold">WELL-COME TO ABHA</span>
@@ -8,4 +8,4 @@ const ScanView = () => {
      );
 };
 
-export default ScanView;
+export default DashboardView;
